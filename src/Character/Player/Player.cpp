e40ef600ec4e57f34072fd372fc2ae6eb2bd21b5
Aman Kumar <aman.kumar01747@gmail.com>
2024-10-22 20:50:16 +0530

    Use Abstract class

--- src/Character/Player/Player.cpp
+++ src/Character/Player/Player.cpp
@@ -3,6 +3,9 @@
 #include "../../include/Pokemon/PokemonChoice.hpp"
 #include "../../include/Pokemon/PokemonType.hpp"
 #include "../../include/Pokemon/Pokemons/Pikachu.hpp"
+#include "../../include/Pokemon/Pokemons/Charmander.hpp"
+#include "../../include/Pokemon/Pokemons/Balbasaur.hpp"
+#include "../../include/Pokemon/Pokemons/Squirtle.hpp"
 #include "../../include/Utility/Utility.hpp"
 #include <iostream>
 
@@ -16,29 +19,31 @@ namespace N_Character
 
         Player::Player() {
             name = "Trainer";
-            chosenPokemon = new Pokemon(); // Using the default Pokemon constructor 
         }
 
-        Player::Player(string p_name, Pokemon* p_chosenPokemon) {
+        Player::Player(string p_name) {
             name = p_name;
-            chosenPokemon = p_chosenPokemon; // Using the default Pokemon constructor 
         }
-
 
         void Player::choosePokemon(int choice)
         {
             //Respond based on player choice
             switch ((PokemonChoice)choice)
             {
-            case PokemonChoice::CHARMANDER:     chosenPokemon = new Pokemon("Charmander", PokemonType::FIRE, 100, 10);
+            case PokemonChoice::CHARMANDER:     
+                chosenPokemon = new Charmander();
                 cout << "You chose Charmander!  A fiery and brave companion." << endl;
                 cout << "Professor Oak: Charmander’s fire will light the path of your journey!" << endl;
                 break;
-            case PokemonChoice::BULBASAUR:     chosenPokemon = new Pokemon("Bulbasaur", PokemonType::GRASS, 100, 8);
+            case PokemonChoice::BULBASAUR:     
+                //chosenPokemon = new Pokemon("Bulbasaur", PokemonType::GRASS, 100, 8);
+                chosenPokemon = new Balbasaur();;
                 cout << "You chose Bulbasaur! A calm and wise choice." << endl;
                 cout << "Professor Oak: Bulbasaur will grow strong, just like your bond with it." << endl;
                 break;
-            case PokemonChoice::SQUIRTLE:     chosenPokemon = new Pokemon("Squirtle", PokemonType::WATER, 100, 9);
+            case PokemonChoice::SQUIRTLE:     
+                //chosenPokemon = new Pokemon("Squirtle", PokemonType::WATER, 100, 9);
+                chosenPokemon = new Squirtle();;
                 cout << "You chose Squirtle! A loyal and dependable friend" << endl;
                 cout << "Professor Oak: Squirtle's water will wash away any obstacles in your way!" << endl;
                 break;
